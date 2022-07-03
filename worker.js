c1e36375514f3fa05f643b43f40bda106c0c033a
Radek Anuszewski <radekqwerty@gmail.com>
2022-07-03 20:40:15 +0200

    Unused code removed

--- worker.js
+++ worker.js
@@ -1,16 +1,10 @@
 self.addEventListener('message', async e => {
-    const { fileName, fileContent, directoryName, type } = e.data;
+    const { fileName, fileContent, directoryName } = e.data;
     const root = await navigator.storage.getDirectory();
     const directoryHandle = await root.getDirectoryHandle(directoryName);
-    if (type === 'write') {
-        const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
-        const fileAccessHandle = await fileHandle.createSyncAccessHandle();
-        const data = new TextEncoder().encode(fileContent);
-        await fileAccessHandle.write(data, { at: 0 });
-        self.postMessage({type: 'written' });
-    }
-    if (type === 'read') {
-        const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
-        const fileAccessHandle = await fileHandle.createSyncAccessHandle();
-    }
+    const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
+    const fileAccessHandle = await fileHandle.createSyncAccessHandle();
+    const data = new TextEncoder().encode(fileContent);
+    await fileAccessHandle.write(data, { at: 0 });
+    self.postMessage('written');
 });
