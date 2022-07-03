dc7d4e3b5e04414ace6456771f9153a7e1bba031
Radek Anuszewski <radekqwerty@gmail.com>
2022-07-03 20:02:28 +0200

    Reading files.

--- worker.js
+++ worker.js
@@ -1,10 +1,16 @@
 self.addEventListener('message', async e => {
-    const { fileName, fileContent, directoryName } = e.data;
+    const { fileName, fileContent, directoryName, type } = e.data;
     const root = await navigator.storage.getDirectory();
     const directoryHandle = await root.getDirectoryHandle(directoryName);
-    const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
-    const fileAccessHandle = await fileHandle.createSyncAccessHandle();
-    const data = new TextEncoder().encode(fileContent);
-    await fileAccessHandle.write(data, { at: 0 });
-    self.postMessage('finished');
+    if (type === 'write') {
+        const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
+        const fileAccessHandle = await fileHandle.createSyncAccessHandle();
+        const data = new TextEncoder().encode(fileContent);
+        await fileAccessHandle.write(data, { at: 0 });
+        self.postMessage({type: 'written' });
+    }
+    if (type === 'read') {
+        const fileHandle = await directoryHandle.getFileHandle(`${fileName}.txt`, { "create" : true });
+        const fileAccessHandle = await fileHandle.createSyncAccessHandle();
+    }
 });
